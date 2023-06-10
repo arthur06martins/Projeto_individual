fdca2f9fd07ba2741959ef21c899d9bd0da61769
arthur06martins <arthur12martins12@gmail.com>
2023-06-10 02:05:20 -0300

    terminei a pagina sobreagentes

--- blogtachanka/site/public/scripts/sobrepersonagem.js
+++ blogtachanka/site/public/scripts/sobrepersonagem.js
@@ -1,13 +1,6 @@
 // const { get } = require("../../src/routes/sobreagentes")
 
 var nomes = ['ace','alibi','amaru','aruni','ash',"azumi",'blackbird','bandit','blitz','castle','brava','caveira','buck','clash','capitão','doc','dokkaebi','echo','finka','ela','flores','frost','fuze','goyo','glaz','jager','gridlock','kaid','grim','kapkan','hibana','lesion','iana','maestro','iq','melusi','jackal','mira','kali','mozzie','lion','mute','maverick','oryx','montagne','pulse','nomad','rook','nook','smoke','osa','solis','sens','tachanka','sledge','thorn','thatcher','thunderbird','thermite','valkyrie','twitch','vigil','ying','wamai','zero','warden','zofia']
-var imagens = [`./img/atacantes/ace.png`,'./img/defesa/alibi.png','./img/atacantes/amaru.png','./img/defesa/aruni.png','./img/atacantes/ash.png','./img/defesa/azami.png','./img/atacantes/blackbird.png',
-'./img/defesa/bandit.png','./img/atacantes/blitz.png','./img/defesa/castle.png','./img/atacantes/brava.png','./img/defesa/caveira.png'
-,'./img/atacantes/buck.png','./img/defesa/clash.png','./img/atacantes/capitao.png','./img/defesa/doc.png','./img/atacantes/dokkaebi.png','./img/defesa/echo.png','./img/atacantes/finka.png','./img/defesa/ela.png','./img/atacantes/flores.png','./img/defesa/frost.png','./img/atacantes/fuze.png',
-'./img/defesa/goyo.png','./img/atacantes/glaz.png','./img/defesa/jager.png','./img/atacantes/gridlock.png','./img/defesa/kaid.png','./img/atacantes/grim.png','./img/defesa/kapkan.png','./img/atacantes/hibana.png','./img/defesa/lesion.png','./img/atacantes/iana.png','./img/defesa/maestro.png','./img/atacantes/iq.png','./img/defesa/melusi.png',
-'./img/atacantes/jackal.png','./img/defesa/mira.png','./img/atacantes/kali.png','./img/defesa/mozzie.png','./img/atacantes/lion.png','./img/defesa/mute.png','./img/atacantes/maverick.png','./img/defesa/oryx.png','./img/atacantes/montagne.png','./img/defesa/pulse.png','./img/atacantes/nomad.png','./img/defesa/rook.png',
-'./img/atacantes/nook.png','./img/defesa/smoke.png','./img/atacantes/osa.png','./img/defesa/solis.png','./img/atacantes/sens.png','./img/defesa/tachanka.png','./img/atacantes/sledge.png','./img/defesa/thorn.png','./img/atacantes/thatcher.png','./img/defesa/thunderbird.png','./img/atacantes/thermite.png','./img/defesa/valkyrie.png','./img/atacantes/twitch.png','./img/defesa/vigil.png',
-'./img/atacantes/ying.png','./img/defesa/wamai.png','./img/atacantes/zero.png','./img/defesa/warden.png','./img/atacantes/zofia.png']
 
 
 
@@ -55,6 +48,11 @@ document.addEventListener('DOMContentLoaded',function ativar() {
                                  
                             if(`${apelido}` == `"${nm}"`)
                             {
+                                DIV_titulo_txt_biografia.innerHTML =`${localStorage.getItem("titulo_biografia").replace(/["]/g,"")}`
+                                DIV_txt_biografia.innerHTML = `${localStorage.getItem("biografia").replace(/[.]/g,`.  `)}`
+                                DIV_local_nasc.innerHTML = `${localStorage.getItem("local_nasc").toUpperCase()}`
+                                DIV_nome_agente.innerHTML = `${localStorage.getItem("nome").toUpperCase()}`
+                                DIV_data_nasc.innerHTML = `${localStorage.getItem("dtNascimento").replace(/[""]/g,"").toUpperCase()}`
                                 imagen_agente.innerHTML=`<img src="./img/agentespng/${nm}.png" alt="">`
                                 img_logo_agente.innerHTML = `<img src="./img/logo_agentes/${nm.replace(/[""]/g,"")}.png" alt="">`
                                 j.innerHTML = `${nm.toUpperCase()}`
@@ -62,38 +60,38 @@ document.addEventListener('DOMContentLoaded',function ativar() {
                                 especialidade.innerHTML =`${localStorage.getItem("especialidade").replace(/[""]/g,"").toLocaleUpperCase()}`
                                 if( saude == 1)
                                 {
-                                    Sradio1.style.background ="blue"
+                                    Sradio1.style.background ="aqua"
                                 
                                 
                                 
                                 }
                                 else if(saude == 2)
                                 {
-                                    Sradio1.style.background ="blue"
-                                    Sradio2.style.background ="blue"
+                                    Sradio1.style.background ="aqua"
+                                    Sradio2.style.background ="aqua"
                                 }
                                 else if(saude == 3)
                                 {
-                                    Sradio1.style.background ="blue"
-                                    Sradio2.style.background ="blue"
-                                    Sradio3.style.background ="blue"
+                                    Sradio1.style.background ="aqua"
+                                    Sradio2.style.background ="aqua"
+                                    Sradio3.style.background ="aqua"
                                 }
                                 //validando a velocidade do agente
                                 if(velocidade == 1)
                                 {
-                                    Vradio1.style.background ="blue"
+                                    Vradio1.style.background ="aqua"
                                 
                                 }
                                 else if(velocidade== 2)
                                 {
-                                    Vradio1.style.background ="blue"
-                                    Vradio2.style.background ="blue"
+                                    Vradio1.style.background ="aqua"
+                                    Vradio2.style.background ="aqua"
                                 }
                                 else if(velocidade == 3)
                                 {
-                                    Vradio1.style.background ="blue"
-                                    Vradio2.style.background ="blue"
-                                    Vradio3.style.background ="blue"
+                                    Vradio1.style.background ="aqua"
+                                    Vradio2.style.background ="aqua"
+                                    Vradio3.style.background ="aqua"
                                 }
 
 
